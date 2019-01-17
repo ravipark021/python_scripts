433a526c13f16a906cbff1e0fb16f24bb74e065d
ravipark021 <ravi.park021@gmail.com>
2019-01-17 19:33:12 +0530

    *correct script

--- framework_migrator.py
+++ framework_migrator.py
@@ -19,9 +19,7 @@ def getProjectFiles(path):
                 f_files.append(os.path.join(path, name))
     return f_files
 
-def parseXMLAndUpdate(xmlfile, mapping_dict):
-    hintPathTag = 'xmlns:HintPath'
-    referenceTag = 'xmlns:ItemGroup/xmlns:Reference'
+def parseXMLAndUpdate(xmlfile, version):
     namespaces = {'xmlns': 'http://schemas.microsoft.com/developer/msbuild/2003'}
     ET.register_namespace('','http://schemas.microsoft.com/developer/msbuild/2003')
     parser = ET.XMLParser(target=CommentedTreeBuilder())
@@ -29,26 +27,24 @@ def parseXMLAndUpdate(xmlfile, mapping_dict):
     
     root = tree.getroot()
     
-    for prop in root.findall(referenceTag, namespaces):
-        for old_val in [key for key, value in mapping_dict.items() if prop.attrib['Include'] == key or key+',' in prop.attrib['Include']]:
-            new_val = mapping_dict[old_val]
-            prop.attrib['Include'] = new_val
-            if prop.find(hintPathTag, namespaces) != None:
-                prop.find(hintPathTag, namespaces).text = prop.find(hintPathTag, namespaces).text.replace(old_val, new_val)
+    print(root)
+    
+    for prop in root.findall('xmlns:PropertyGroup/xmlns:TargetFrameworkVersion', namespaces): 
+        prop.text = version
     tree.write(xmlfile, encoding='utf-8', xml_declaration=True);
 
-def referencesUpdator(path, mapping_dict):
+def targetFrameworkMigrate(path, version):
     files = getProjectFiles(path)
     for f in files:
-        parseXMLAndUpdate(f, mapping_dict)
+        parseXMLAndUpdate(f, version)
 
 def main():
     if len(sys.argv) < 2:
         print('invalid arguments')
-        print('must contain atleast one mapping in the form of <existing_assembly>=<updated_assembly> separated by space(if multiple)')
+        print('must provide "-path=<project_dir>" and "-version=<target_framework>" parameters')
         return
+    
     params_dict = {}
-    mapping_dict = {}
     for i in range(1, len(sys.argv)):
         key, value = sys.argv[i].split('=')
         params_dict[key] = value
@@ -60,16 +56,13 @@ def main():
         print('path param is not provided')
         return
     
-    try:
-        for i in range(2, len(sys.argv)):
-            arg_split = sys.argv[i].split('=')
-            mapping_dict[arg_split[0]] = arg_split[1]
-    except:
-        print('error in mappings format, please check and try again')
-        
+    if '-version' in params_dict.keys():
+        version = params_dict['-version']
+    else:
+        print('version param is not provided')
+        return
     
-    referencesUpdator(path, mapping_dict)
-    print('Done! References Updated!')
+    targetFrameworkMigrate(path, version)
     
 if __name__ == '__main__':
     main()
